57c039fd983e1acaa1c9b211dc795413d98137c4
Chu-4hun <tapok134224@mail.ru>
2022-04-08 11:25:41 +0300

    merge

--- main.py
+++ main.py
@@ -55,24 +55,7 @@ def StrCounter():
     except Exception:
         print("Ошибка ввода данных")
 
-def Matrix():
-    try:
-        a = int(input("Кол-во столбцов в матрице: "))
-        b = int(input("Кол-во строк в матрице: "))
-        c = int(input("Начальное число: "))
-        d = int(input("Шаг: "))
-        matrix = []
-        for i in range(b):
-            matrix.append([])
-            for j in range(a):
-                matrix[i].append(c)
-                c += d
-        for i in range(b):
-            for j in range(a):
-                print(matrix[i][j], end = " ")
-            print("")
-    except Exception:
-        print("Ошибка ввода данных")
+
 
 def main():
     while True:
